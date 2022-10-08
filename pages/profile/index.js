b2e6730402dcca2ce862e01eccb23168b4e4dbf2
Nikita-Vrsg <nikitavrsgn@gmail.com>
2022-10-08 21:52:45 +0300

    Добавил универсальный page-title

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,11 +1,21 @@
-import { Flex } from "@chakra-ui/react";
 import React from "react";
+import { Flex } from "@chakra-ui/react";
 import { SideMenu } from "../../components/SideMenu";
+import { PageTitle } from "../../components/PageTitle";
 
 const ProfilePage = () => {
   return (
     <Flex bgColor="#F7FAFC" height="100vh" width="100%">
       <SideMenu />
+      <Flex mt="35px" ml="20px">
+        <PageTitle
+          iconWidth={70}
+          iconHeight={90}
+          title={`Приветствую, ${"Карлен"}`}
+          fontSize="36px"
+          icon="/coat.svg"
+        />
+      </Flex>
     </Flex>
   );
 };
